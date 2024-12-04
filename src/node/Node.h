0281278886dd2535e12bc4dc7ce72b69529d778c
Bryannsss <bninamango.personal@gmail.com>
2024-12-04 14:02:59 -0500

    feat: Add Trie and Node

--- src/node/Node.h
+++ src/node/Node.h
@@ -5,12 +5,7 @@
 #ifndef NODE_H
 #define NODE_H
 
-
-
 class Node {
-
 };
-
-
 
 #endif //NODE_H
--- src/trie/Trie.h
+++ src/trie/Trie.h
@@ -5,12 +5,9 @@
 #ifndef TRIE_H
 #define TRIE_H
 
-
-
 class Trie {
-
+public:
+    Trie();
 };
-
-
 
 #endif //TRIE_H
